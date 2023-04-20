de76b56353bd0413757f860f854c2f2653a18d3f
Antonio Jimenez Gonzalez <anjimenewebmaster@hotmail.com>
2023-04-20 17:11:35 +0200

    Refactored main code

---
+++ main.py
@@ -0,0 +1,166 @@
+#import sklearn as sk
+import pickle
+
+import os, sys
+import numpy as np
+import pandas as pd
+import re
+import pickle
+import iso639
+import time
+from collections import Counter
+from tqdm import tqdm
+from pathlib import Path
+
+from process import main_process
+
+os.environ["NLTK_DATA"] = "./utils/"
+
+main_process(True,'stemming','nltk','../../Calibre','output')
+
+with open('final.pickle', 'rb') as handle:
+    final = pickle.load(handle)
+
+final['metadata'].head()
+
+final['words'].head()
+
+print('Shape of metadata DF: '+str(final['metadata'].shape))
+print('\nShape of words DF: '+str(final['words'].shape))
+
+ax=final['metadata']['length_full'].sort_values().plot(logy=True)
+a=np.round(np.linspace(0,final['metadata'].shape[0],15))
+ax.xaxis.set_ticks(a)
+ax.set_xticklabels(a)
+plt.xlabel('Number of books')
+plt.ylabel('Length of books')
+
+
+values = (100*final['metadata']['length_unique']/final['metadata']['length_full']).sort_values(ascending=False)
+authors = final['metadata']['author'].loc[values.index].values
+subjects = final['metadata']['subject'].loc[values.index].values
+df_length = pd.DataFrame(list(zip(authors,values,subjects)), columns=['Authors','Ratio','Subjects'])
+df_length = df_length.groupby('Authors').agg({'Ratio':'median', 'Subjects':lambda col: ','.join(col)})
+
+def my_func(x):
+    return(Counter(x.split(',')).most_common()[0][0])
+subjects = [my_func(x) for x in df_length['Subjects']]
+
+dd = defaultdict(lambda: 'b')
+dd['Science-Fiction'] = 'r'
+dd['Fantasy'] = 'r'
+dd['Littérature'] = 'g'
+dd['Historique'] = 'y'
+dd['Histoire'] = 'y'
+dd['Philosophie'] = 'g'
+dd['Roman historique'] = 'y'
+dd['Romans historiques'] = 'y'
+
+df_length['Subjects'] = subjects
+df_length.reset_index().sort_values('Ratio', ascending=False).plot.bar(x='Authors', y='Ratio',
+                                     color= df_length['Subjects'].map(dd))
+
+sf = mpatches.Patch(color='r', label='Science-Fiction, Fantasy')
+li = mpatches.Patch(color='g', label='Littérature, Philosophie')
+hi = mpatches.Patch(color='y', label='Histoire, Historique')
+bl = mpatches.Patch(color='b', label='Autre')
+plt.ylabel('Ratio unique words/full length')
+_=plt.legend(handles=[sf, li, hi, bl])
+
+list(final['words'].index)
+
+def plot_wordcloud(data):
+    from wordcloud import WordCloud
+    wordcloud2 = WordCloud(background_color='white',
+                           max_words=400,
+                           max_font_size=80,
+                           width=800, height=400,
+                           normalize_plurals=False
+                             )
+    wordcloud2.generate_from_frequencies(data.to_dict())
+    fig = plt.figure(figsize = (40,20))
+    plt.imshow(wordcloud2, interpolation='bilinear')
+    fig.suptitle(data.name, fontsize=70)
+
+number = int([i for i, s in enumerate(final['words'].index) if 'Candide' in s][0])
+data = final['words'].iloc[number]
+data.sort_values(ascending=False)
+
+final['words']['homme']
+
+# FICTION
+number = int([i for i, s in enumerate(final['words'].index) if 'Candide' in s][0])
+data = final['words'].iloc[number]
+plot_wordcloud(data)
+
+number = int([i for i, s in enumerate(final['words'].index) if 'Fondation' in s][1])
+data = final['words'].iloc[number]
+plot_wordcloud(data)
+
+# NON-FICTION
+number = int([i for i, s in enumerate(final['words'].index) if 'Sapiens' in s][0])
+data = final['words'].iloc[number]
+plot_wordcloud(data)
+
+number = int([i for i, s in enumerate(final['words'].index) if 'Mousquetaires' in s][0])
+data = final['words'].iloc[number]
+plot_wordcloud(data)
+
+number = int([i for i, s in enumerate(final['words'].index) if 'Kilo' in s][0])
+data = final['words'].iloc[number]
+plot_wordcloud(data)
+
+plot_wordcloud(final['words'].sum(axis=0))
+
+final['words'].mean(axis=0).sort_values(ascending=False)
+
+[x for x,y in Counter(final['words'].columns).items() if y==1]
+
+nr=[i for i, s in enumerate(final['words'].index) if 'Mousquetaires' in s][0]
+
+from sklearn.metrics.pairwise import pairwise_distances
+from scipy.spatial.distance import pdist
+
+h = pairwise_distances(final['words'], metric='cosine') #cosine, cityblock, correlation
+#h = pdist(final['words'], metric='cosine')
+
+import plotly
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+plotly.tools.set_credentials_file(username='aoning', api_key='pXneTE0EeDB2YsrRCEZo')
+
+numbi = len(final['words'].index)
+numbi = 500
+
+trace = go.Heatmap(z=h[0:numbi,0:numbi],
+                   x=final['words'].index[0:numbi],
+                   y=final['words'].index[0:numbi], reversescale = True)
+
+
+data=[trace]
+py.iplot(data, filename='labelled-heatmap')
+
+from scipy.cluster import hierarchy
+from scipy.spatial.distance import squareform
+
+link = hierarchy.linkage(h[0:numbi], method='single')
+o1 = hierarchy.leaves_list(link)
+
+mat = h[o1,:]
+mat = mat[:, o1[::-1]]
+mat = mat[::-1,:]
+labs_mat = final['words'].index[o1]
+labs_mat = labs_mat[::-1]
+
+trace = go.Heatmap(z=mat[0:numbi,0:numbi],
+                   x=labs_mat[0:numbi],
+                   y=labs_mat[0:numbi], reversescale = True)
+
+
+data=[trace]
+py.iplot(data, filename='labelled-heatmap')
+
+
--- process.py
+++ process.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import textract, os, sys
 import numpy as np
 import pandas as pd
@@ -8,164 +5,54 @@ import re
 import pickle
 import iso639
 import time
+import argparse
+import logging
 from collections import Counter, OrderedDict
-from tqdm import tqdm
-from pathlib import Path
+from xml.etree import ElementTree as ET
+from typing import List, Dict, Tuple
 
-#Path('./running').touch()
-print('Start process')
+class Application:
+    def __init__(self, data_path: str, sample: int = -1):
+        self.data_path = data_path
+        self.authors = [x for x in os.listdir(self.data_path) if os.path.isdir(os.path.join(self.data_path, x))]
+        logging.debug(f"Detected authors: {sorted(self.authors)}")
+        if not self.authors:
+            logging.warning(f"No books detected")
+        else:
+            self.files: List[str] = []
+            for root, dirs, files in os.walk(self.data_path):
+                for file in files:
+                    if file.endswith('.epub'):
+                        self.files.append(os.path.join(root, file))
+            if sample > 0:
+                self.files = self.files[0:sample]
+            self.books = [Book(path=x) for x in self.files]
 
-def main_process(verbose, method, stopWords, ebookPath, outputPath):    
-    def extract_metadata(path, author, book):
-        # 1) Get metadata info from .opf file
-        #language, title, subject
-        from xml.etree import ElementTree as ET
-        
-        metadata = dict()
-        
-        dirname = "/".join([path,author,book])
-        tree = ET.parse("/".join([dirname,'metadata.opf']))
-        #extract title, subject and language
-        for x in ["title","subject","language"]:
-            temp = [x.text for x in tree.findall(".//{http://purl.org/dc/elements/1.1/}"+x)]
-            metadata[x] = temp if np.size(temp) == 1 else [",".join(temp)]
-        metadata['author'] = [author]
-        metadata['language'] = [str.lower(re.findall(r'\w[\w-]+', iso639.to_name(metadata['language'][0]))[0])]
-        return metadata
-    
-    def extract_words(path, author, book, metadata, method, stopwords): # method: lemmatization or stemming, stopwords : spacy or nltk
-        # 2) Open ebook, parse text, extract words (BoW) 
-        
-        final = dict()
-        text = []
-        
-        dirname = "/".join([path,author,book])
-        temp = os.listdir(dirname) 
-        book = [x for x in temp if x.endswith('.epub')]
-        if not book: # Only process epub books
-            if([x for x in temp if x.endswith('.cbz')] or [x for x in temp if x.endswith('.cbr')]):
-                print("Comic booc found")
-            else:
-                print("No epub found!")
-        else:
-            book = book[0]
-            book_path = "/".join([dirname,book])
-            # Extract text, find words, put in an ordered Dict
-            try:
-                text = textract.process(book_path)
-            except:
-                print("Problem with book!: " + author + ", " + book)
-            else:
-                text = str(text,"utf-8")
-                text = text.replace(u'\xad', '') # see below
-                #These are characters that mark places where a word could be split when fitting lines to a page. 
-                #The idea is that the soft hyphen is invisible if the word doesn't need to be split, but printed the same as a U+2010 normal hyphen if it does.
-                text = text.replace(u'\xa0', u' ')
-                text = re.findall(r'\w[\w-]+', text)
-                text = [x.lower() for x in text]
-                ###############
-                if(method == 'stemming'):
-                    from nltk.stem import SnowballStemmer
-                    stemmer = SnowballStemmer(metadata['language'][0])
-                    text = [stemmer.stem(word) for word in text]
-                elif(method == 'lemmatization'):
-                    import spacy
-                    nlp = spacy.load(iso639.to_iso639_1(metadata['language'][0]))
-                    p_text = " ".join(text)
-                    nlp.max_length = len(p_text) + 50
-                    doc = nlp(p_text, disable=['parser', 'tagger', 'ner'])
-                    text = [token.lemma_ for token in doc]
-                elif(method == 'none'):
-                    pass
-                else:
-                    raise ValueError("method must be either 'stemming', 'lemmatization' or 'none' !")
-                ###############
-                customized_stop_words = ('xhtml', '-', 'qu')
-                if(stopwords=='nltk'):   
-                    import nltk
-                    from nltk.corpus import stopwords
-                    nltk.data.path.append("/media/hdd/jupyter-notebooks/ebook_project/utils")
-                    stopwords = set(stopwords.words(metadata['language'][0]))
-                    stopwords.update(customized_stop_words)
-       
-                elif(stopwords=='spacy'):
-                    import spacy
-                    stopwords = getattr(spacy.lang, iso639.to_iso639_1(metadata['language'][0])).stop_words.STOP_WORDS
-                    stopwords.update(customized_stop_words)
-    
-                else:
-                    stopwords = set(customized_stop_words)
-                
-                text = [x for x in text if x not in stopwords]
-                # keep only words
-                masking = [False if re.match(r'^[^a-z]*[0-9]+[^a-z]*$',x) else True for x in text]
-                text = [i for (i, v) in zip(text, masking) if v]
-                    
-                d = dict(Counter(text).items())
-                final = OrderedDict(sorted(d.items(), key=lambda x: x[1], reverse = True))
-        return text, final
-        
-    def process_book(path, author, book, method, stopwords, verbose=True):
-        # Function that reads each ebook, parses the text and performs stemming and/or stopwords removal if the option is selected
-        # Returns two dictionaries: 1) metadata: contatining metadata of the book, and final: which contains the words from the book
-        
-        metadata = dict()
-        words = dict()
-        text = []
-        
-        if(verbose):
-            print("\nProcessing " + book)
-        ################################################
-        # 1) Get metadata info from .opf file
-        #language, title, subject
-        metadata = extract_metadata(path, author, book)
-        if(verbose):
-            for x in metadata.keys():
-                print(x + ': ' + metadata[x][0])
-        if(metadata['language'][0] == 'french'):
-        ################################################
-# 2) Open ebook, parse text, extract words (BoW)  
-            text, words = extract_words(path, author, book, metadata, method, stopwords)
-        
-        ################################################
-        # 3) Set other metadata info  
-        if(book and text):
-            metadata['length_unique'] = [len(set(text))]
-            metadata['length_full'] = [len(text)]
-        else:
-            metadata['length_unique'] = [0]
-            metadata['length_full'] = [0]
-            
-        if(verbose):
-            print('full length: ' + str(metadata['length_full'][0]))
-            print('unique length: ' + str(metadata['length_unique'][0]))
-        ################################################
-        return words, metadata
-    
-    def refine_dict(df, lang = 'french'):
+
+    def refine_dict(df, lang='french'):
         # 1) Drops comic books (BD), or Audiobooks,
         # 2) drops books based on the language,
         # 3) drops books based on whether it could detect words or not
         # Only keep books for specific language, 'fra' by default since most of the books I have are in French
-        
+
         ######  1) REMOVE AUDIOBOOKS OR COMIC BOOKS
-        rows_to_remove = df['metadata'].index[np.where((df['metadata']['subject']=='Audiobook') | 
-                                                       (df['metadata']['subject']=='BD'))[0]]
+        rows_to_remove = df['metadata'].index[np.where((df['metadata']['subject'] == 'Audiobook') |
+                                                       (df['metadata']['subject'] == 'BD'))[0]]
         for i in df.keys():
             df[i].drop(rows_to_remove, axis=0, inplace=True)
         #######  2) LANGUAGE
-        rows_to_remove = df['metadata'].index[np.where(df['metadata']['language']!=lang)[0]]
+        rows_to_remove = df['metadata'].index[np.where(df['metadata']['language'] != lang)[0]]
         for i in df.keys():
             df[i].drop(rows_to_remove, axis=0, inplace=True)
         #######  3) BOOKS WITH NOT ENOUGH WORDS
         # how to know whether a book has not enough words? Most of the books are epub format, words can be easily scrapped,
-        # some books are pdf and the textract process does not necesarily extract them. 
-        # As such we will remove them as a first naive approach. Effectively, this means removing boosk with length_full of 0 
-        # up to 900 
-        rows_to_remove = df['metadata'].index[np.where(df['metadata']['length_full']<900)[0]]
+        # some books are pdf and the textract process does not necesarily extract them.
+        # As such we will remove them as a first naive approach. Effectively, this means removing boosk with length_full of 0
+        # up to 900
+        rows_to_remove = df['metadata'].index[np.where(df['metadata']['length_full'] < 900)[0]]
         for i in df.keys():
             df[i].drop(rows_to_remove, axis=0, inplace=True)
-            
+
         #####  4) NOW WE WILL REMOVE THE WORDS THAT ARE NUMBERS (The probably are page numbers, or others. We dont need them)
         # We will for instance remove '112' or '12-13' but not 'paris12', which we will keep stripping the numbers
         cols_to_remove = [x for x in df['words'].columns if re.findall(r'^[^a-z]*[0-9]+[^a-z]*$', x)]
@@ -173,7 +60,7 @@ def main_process(verbose, method, stopWords, ebookPath, outputPath):
         # Now strip numbers from column names
         cols_to_rename = [re.sub(r'[\d]+', '', x) for x in df['words'].columns]
         df['words'].columns = cols_to_rename
-        
+
         ####  5) REMOVE COLUMNS WITH ONLY NANS IN THE WORDS DF
         cols_to_remove = [x for (x, v) in zip(df['words'].columns, df['words'].isna().all(axis=1)) if v]
         df['words'].drop(cols_to_remove, axis=1, inplace=True)
@@ -187,23 +74,124 @@ def main_process(verbose, method, stopWords, ebookPath, outputPath):
 
         ####  8) MERGE COLUMNS WITH THE SAME NAME --> SINCE THEY HAVE BEEN LEMATIZED
         df['words'] = df['words'].groupby(level=0, axis=1).sum()
-    
+
     def compute_IDF(df):
         # DF is a PD dataframe with rows as documents, and columns as tokens
         from sklearn.feature_extraction.text import TfidfTransformer
-        tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
+        tfidf_transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
         tfidf_transformer.fit(df)
-        #df_idf = pd.DataFrame(tfidf_transformer.idf_,index=df.columns, columns=["tf_idf_weights"])    
+        # df_idf = pd.DataFrame(tfidf_transformer.idf_,index=df.columns, columns=["tf_idf_weights"])
         return tfidf_transformer.idf_
+
     def compute_TF(df):
         # DF is a PD dataframe with rows as documents, and columns as tokens
         return df.div(df.sum(axis=1), axis=0)
-    
+
+
+class Book:
+    def __init__(self, path: str, method='lemmatization', stopwords='spacy'):
+        """
+        Class constructor
+        :param path: Absolute path to the epub file of the book
+        """
+        self.path = os.path.dirname(path)
+        self.book_filename = os.path.basename(path)
+        self.author = os.path.basename(os.path.dirname(self.path))
+        self.language, self.subject, self.title = self._extract_metadata()
+        self.text, self.final, self.length_book_unique, self.length_book_full = self._extract_words(method=method, stopwords=stopwords)
+
+    def __str__(self):
+        return f'Filename: {self.book_filename}, Author: {self.author}, Path: {self.path}, ' \
+               f'Language: {self.language}, Subject: {self.subject}, Title: {self.title}'
+
+    def _extract_metadata(self) -> Tuple[str, str, str]:
+        """
+        This method will extract the Calibre metadata from the epub : language, title, subject
+        :return metadata: Metadata dictionnary
+        """
+        metadata: Dict[str, str] = {}
+        tree = ET.parse(os.path.join(self.path, 'metadata.opf'))
+        for x in ["title", "subject", "language"]:
+            field = [x.text for x in tree.findall(".//{http://purl.org/dc/elements/1.1/}" + x)]
+            metadata[x] = field if len(field) == 1 else [",".join(field)]
+        metadata['language'] = [str.lower(re.findall(r'\w[\w-]+', iso639.to_name(metadata['language'][0]))[0])]
+        return metadata['language'], metadata['subject'], metadata['title']
+
+    def _extract_words(self, method: str, stopwords: str) -> Tuple[List[str], OrderedDict, int, int]:
+        """
+        Open ebook, parse text, extract words (BoW)
+        :param method:
+        :param stopwords:
+        :return:
+        """
+        final = dict()
+        text = []
+
+        # Extract text, find words, put in an ordered Dict
+        try:
+            text = textract.process(os.path.join(self.path, self.book_filename))
+        except:
+            print("Problem with book!: " + self.author + ", " + self.path)
+        else:
+            text = str(text, "utf-8")
+            text = text.replace(u'\xad', '')  # see below
+            # These are characters that mark places where a word could be split when fitting lines to a page.
+            # The idea is that the soft hyphen is invisible if the word doesn't need to be split, but printed the same as a U+2010 normal hyphen if it does.
+            text = text.replace(u'\xa0', u' ')
+            text = re.findall(r'\w[\w-]+', text)
+            text = [x.lower() for x in text]
+            ###############
+            if method == 'stemming':
+                from nltk.stem import SnowballStemmer
+                stemmer = SnowballStemmer(self.language)
+                text = [stemmer.stem(word) for word in text]
+            elif method == 'lemmatization':
+                import spacy
+                #nlp = spacy.load(iso639.to_iso639_1(self.language[0]))
+                # python -m spacy download fr_dep_news_trf
+                nlp = spacy.load("fr_dep_news_trf")
+                p_text = " ".join(text)
+                nlp.max_length = len(p_text) + 50
+                doc = nlp(p_text, disable=['parser', 'tagger', 'ner'])
+                text = [token.lemma_ for token in doc]
+            elif method == 'none':
+                pass
+            else:
+                raise ValueError("method must be either 'stemming', 'lemmatization' or 'none' !")
+            ###############
+            customized_stop_words = ('xhtml', '-', 'qu')
+            if stopwords == 'nltk':
+                import nltk
+                from nltk.corpus import stopwords
+                nltk.data.path.append("/utils")
+                stopwords = set(stopwords.words(self.language[0]))
+                stopwords.update(customized_stop_words)
+
+            elif stopwords == 'spacy':
+                import spacy
+                stopwords = getattr(spacy.lang,
+                                    iso639.to_iso639_1(self.language[0])).stop_words.STOP_WORDS
+                stopwords.update(customized_stop_words)
+
+            else:
+                stopwords = set(customized_stop_words)
+
+            text = [x for x in text if x not in stopwords]
+            # keep only words
+            masking = [False if re.match(r'^[^a-z]*[0-9]+[^a-z]*$', x) else True for x in text]
+            text = [i for (i, v) in zip(text, masking) if v]
+
+            d = dict(Counter(text).items())
+            final = OrderedDict(sorted(d.items(), key=lambda x: x[1], reverse=True))
+
+            length_book_unique = len(set(text))
+            length_book_full = len(text)
+
+        return text, final, length_book_unique, length_book_full
+
+
+def main_process(verbose, method, stopWords, ebookPath, outputPath):
     # MAIN LOOP
-    path = ebookPath
-    authors = [x for x in os.listdir(path) if os.path.isdir("/".join([path,x]))]
-    authors.remove('CALM')
-    
     counter = 0
     metadata_dict = dict()
     words_list = []
@@ -255,3 +243,16 @@ def main_process(verbose, method, stopWords, ebookPath, outputPath):
     #os.remove('./runnning')
     print('Process ended')
     return to_return
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--log_level", help="Log level [DEBUG, INFO, WARNING, ERROR, CRITICAL]",
+                        default="INFO", action="store")
+    args = parser.parse_args()
+    format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
+    log_level = getattr(logging, args.log_level)
+    logging.basicConfig(level=log_level, format=format)
+
+    app = Application(data_path='test_files', sample=2)
+    book = Book(path='test_files/Aldous Huxley/Le Meilleur Des Mondes (72)/Le Meilleur Des Mondes - Aldous Huxley.epub')
